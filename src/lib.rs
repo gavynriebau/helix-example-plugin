25703d7b10eae91234130e214591e73bf71dc138
Gavyn Riebau <gavyn.riebau@gmail.com>
2022-07-06 19:49:32 +0800

    Log messages back to host
    The "helix_log" function is used to pass a log message back a helix
    runtime host function where it is logged.

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,33 @@
 use protobuf::Message;
 use std::mem;
-use std::os::raw::c_void;
+use std::os::raw::{c_char, c_void};
 
 use crate::generated::messages::event::EventType;
 use crate::generated::messages::{Event, KeyEvent};
 
 mod generated;
 
+#[link(wasm_import_module = "helix")]
+extern "C" {
+    pub fn log(pointer: *mut c_char, len: usize);
+}
+
+fn helix_log(msg: &str) {
+    let text = String::from(msg);
+    let ptr: *const u8 = text.as_ptr();
+    let len: usize = text.len();
+
+    unsafe {
+        log(ptr as *mut i8, len);
+    }
+}
+
 // Plugins must provide allocate and deallocate functions that helix can use for passing data back/forth.
 // See: https://github.com/wasmerio/wasmer/issues/1449
 
 #[no_mangle]
 pub extern "C" fn allocate(size: usize) -> *mut c_void {
-    println!("allocate called with size: {}", size);
+    helix_log(&format!("allocate called with size: {}", size));
     let mut buffer = Vec::with_capacity(size);
     let pointer = buffer.as_mut_ptr();
     mem::forget(buffer);
@@ -22,10 +37,10 @@ pub extern "C" fn allocate(size: usize) -> *mut c_void {
 
 #[no_mangle]
 pub extern "C" fn deallocate(pointer: *mut c_void, capacity: usize) {
-    println!(
+    helix_log(&format!(
         "deallocate called with pointer '{:?}' and capacity '{}'",
         pointer, capacity
-    );
+    ));
 
     unsafe {
         let _ = Vec::from_raw_parts(pointer, 0, capacity);
@@ -40,11 +55,11 @@ pub extern "C" fn handle_event(pointer: *mut u8, len: usize) {
 
     match event_type {
         EventType::PLUGIN_STARTED => {
-            println!("Received PLUGIN_STARTED event");
+            helix_log("Received PLUGIN_STARTED event");
         }
         EventType::KEY_EVENT => {
             let key_event: KeyEvent = KeyEvent::parse_from_bytes(&event.payload).unwrap();
-            println!("Received KEY_EVENT: {:#?}", key_event);
+            helix_log(&format!("Received KEY_EVENT: {:#?}", key_event));
         }
     }
 }
